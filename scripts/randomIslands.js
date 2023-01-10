d7c03b7ced833c26b9d195dfe972421f7186429e
Robin500818167 <robin.van.houten@hva.nl>
2023-01-10 12:07:34 +0100

    Making import function

---
+++ scripts/randomIslands.js
@@ -0,0 +1,22 @@
+async function randomIsland() {
+    for (let i = 1; i <= 10; i++) {
+        const SVG_NS = 'http://www.w3.org/2000/svg';
+        const SVG_XLINK = "http://www.w3.org/1999/xlink";
+        //create a new use element
+        let use = document.createElementNS(SVG_NS, 'use');
+        // set the value for 'xlink:href' of the new use element
+        use.setAttributeNS(SVG_XLINK, 'xlink:href', '#poly');
+        //the random width 
+        let w = Math.random()*50;
+        // the proportiopnal height
+        let h = w*83.3 / 75;
+        //set the position and the size of the use element
+        use.setAttributeNS(null, 'x', Math.random()*(500 - w));
+        use.setAttributeNS(null, 'y', Math.random()*(250 - h));
+        use.setAttributeNS(null, 'width', w);
+        use.setAttributeNS(null, 'height', h);
+        //Append the use element
+            svg.appendChild(use); 
+        }
+}
+export default randomIsland;
