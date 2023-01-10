8b005bf58c7714cc0c8c9492ad160ec0fff26fd1
Robin500818167 <robin.van.houten@hva.nl>
2023-01-10 11:53:06 +0100

    Random islands

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,24 @@
 // Our bundler automatically creates styling when imported in the main JS file!
 import '../styles/style.css'
+for (let i = 1; i <= 10; i++) {
+const SVG_NS = 'http://www.w3.org/2000/svg';
+const SVG_XLINK = "http://www.w3.org/1999/xlink";
+//create a new use element
+let use = document.createElementNS(SVG_NS, 'use');
+// set the value for 'xlink:href' of the new use element
+use.setAttributeNS(SVG_XLINK, 'xlink:href', '#poly');
+//the random width 
+let w = Math.random()*50;
+// the proportiopnal height
+let h = w*83.3 / 75;
+//set the position and the size of the use element
+use.setAttributeNS(null, 'x', Math.random()*(500 - w));
+use.setAttributeNS(null, 'y', Math.random()*(250 - h));
+use.setAttributeNS(null, 'width', w);
+use.setAttributeNS(null, 'height', h);
+//Append the use element
 
-// We can use node_modules directely in the browser!
-import * as d3 from 'd3';
-import count from './count.js';
 
-console.log('Hello, world!');
 
-let data = count(5, 5);
-
-console.log(data);
+    svg.appendChild(use); 
+}
