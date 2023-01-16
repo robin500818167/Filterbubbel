ec39da279c77f22be19c47743e1970bfdae625d5
Robin500818167 <robin.van.houten@hva.nl>
2023-01-16 21:45:25 +0100

    Changing ranges

--- scripts/main.js
+++ scripts/main.js
@@ -5,5 +5,13 @@ randomIsland();
 import randomBottle from './randomBottles.js';
 randomBottle();
   
- 
+var elem = document.querySelector('input[type="range"]');
+
+var rangeValue = function(){
+  var newValue = elem.value;
+  var target = document.querySelector('.value');
+  target.innerHTML = newValue;
+}
+
+elem.addEventListener("input", rangeValue);
 
