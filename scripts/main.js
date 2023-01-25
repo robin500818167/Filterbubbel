74a57da453c0a279d7c514ce67d9408a3c00833e
Robin500818167 <robin.van.houten@hva.nl>
2023-01-25 15:59:54 +0100

    animation

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,34 @@
-// import randomIsland from './randomIslands.js';
-// randomIsland();
-// import randomBottle from './randomBottles.js';
-// randomBottle();
-// var elem = document.querySelector('input[type="range"]');
-//   var newValue = elem.value;
-//   // var target = document.querySelector('.value');
-//   // target.innerHTML = newValue;
-//   console.log(newValue)
-// elem.addEventListener("input");
 function inputRange() {
   var field1 = document.getElementById("field").value;
   localStorage.setItem('rangeValue', field1);
+  console.log(field1)
 }
 function changeDelay() {
   var v = localStorage.getItem('rangeValue'); 
 
   if (v < 25) //zacht
    {
-    document.getElementById("kat").style.animationDelay = "8s";
-    document.getElementById("hond").style.animationDelay = "12s";
-    document.getElementById("konijn").style.animationDelay = "16s";
+    console.log("zacht")
+    document.getElementById("kat").style.animationDelay = "2s";
+    document.getElementById("hond").style.animationDelay = "6s";
+    document.getElementById("konijn").style.animationDelay = "10s";
   }
   if (v >= 25) //hard
   {
-    console.log("test2")
-    document.getElementById("kat").style.animationDelay = "8s";
-
-    document.getElementById("hond").style.animationDelay = "10s";
+    console.log(v)
+    document.getElementById("kat").style.animationDelay = "2s";
+    document.getElementById("hond").style.animationDelay = "4s";
     var hEi = document.querySelectorAll('.hEi');
     for (var i = 0; i < hEi.length; i++) {
     var currenthEi = hEi[i];
-    currenthEi.style.animation = "gone 1.0s forwards 10s";
+    currenthEi.style.animation = "gone 1.0s forwards 4s";
     }
-    document.getElementById("konijn").style.animationDelay = "6s";
+    document.getElementById("konijn").style.animationDelay = "0s";
     var kEi = document.querySelectorAll('.kEi');
     for (var i = 0; i < kEi.length; i++) {
     var currentkEi = kEi[i];
-    currentkEi.style.animation = "gone 1.0s forwards 6s";
+    currentkEi.style.animation = "gone 1.0s forwards 0s";
     }
-    document.getElementById("popup1").style.animationDelay = "13s";
+    document.getElementById("popup1").style.animationDelay = "7s";
   }
 }
