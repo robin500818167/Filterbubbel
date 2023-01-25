038741dd762506b9e78aa212949c683da8c89a4e
Robin500818167 <robin.van.houten@hva.nl>
2023-01-25 13:31:47 +0100

    animation

--- scripts/main.js
+++ scripts/main.js
@@ -12,20 +12,32 @@ function inputRange() {
   var field1 = document.getElementById("field").value;
   localStorage.setItem('rangeValue', field1);
 }
-
 function changeDelay() {
   var v = localStorage.getItem('rangeValue'); 
-console.log(document.getElementById("island"));
-  if (v < 3)
-  {
-    document.getElementById("kat").style.animationDelay = "3s";
-    document.getElementById("hond").style.animationDelay = "3s";
-    document.getElementById("konijn").style.animationDelay = "3s";
+
+  if (v < 25) //zacht
+   {
+    document.getElementById("kat").style.animationDelay = "8s";
+    document.getElementById("hond").style.animationDelay = "12s";
+    document.getElementById("konijn").style.animationDelay = "16s";
   }
-  if (v > 3)
+  if (v >= 25) //hard
   {
-    document.getElementById("kat").style.animationDelay = "10s";
+    console.log("test2")
+    document.getElementById("kat").style.animationDelay = "8s";
+
     document.getElementById("hond").style.animationDelay = "10s";
-    document.getElementById("konijn").style.animationDelay = "10s";
+    var hEi = document.querySelectorAll('.hEi');
+    for (var i = 0; i < hEi.length; i++) {
+    var currenthEi = hEi[i];
+    currenthEi.style.animationDelay = "10s";
+    }
+    document.getElementById("konijn").style.animationDelay = "6s";
+    var kEi = document.querySelectorAll('.kEi');
+    for (var i = 0; i < kEi.length; i++) {
+    var currentkEi = kEi[i];
+    currentkEi.style.animationDelay = "6s";
+    }
+    document.getElementById("popup1").style.animationDelay = "13s";
   }
 }
